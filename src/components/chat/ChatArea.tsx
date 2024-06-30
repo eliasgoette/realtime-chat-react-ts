266074aeb2a919740b000b169cc3fcaa7b6666bc
Elias G <83478605+eliasgoette@users.noreply.github.com>
2024-06-30 21:45:41 +0200

    Enhanced chat design

--- src/components/chat/ChatArea.tsx
+++ src/components/chat/ChatArea.tsx
@@ -5,47 +5,47 @@ import ChatBubble, { chatMessage } from "./ChatBubble";
 const ChatArea : FC = () => {
     let messages : chatMessage[] = [
         {
-            content: "Foo",
+            content: "Kale chips single-origin coffee slow-carb, bruh helvetica salvia.",
             timestamp: Date.now() - 8,
             authorUid: "U1"
         },
         {
-            content: "Bar",
+            content: "Praxis pabst chia master cleanse fit tofu.",
             timestamp: Date.now() - 7,
             authorUid: "U2"
         },
         {
-            content: "Whatevs",
+            content: "Seitan same umami banh mi helvetica, before they sold out activated charcoal deep v hella shaman bodega boys sriracha dreamcatcher.",
             timestamp: Date.now() - 6,
             authorUid: "U1"
         },
         {
-            content: "Check",
+            content: "Kombucha single-origin coffee vape lo-fi DIY.",
             timestamp: Date.now() - 5,
             authorUid: "U2"
         },
         {
-            content: "It out",
+            content: "Mixtape iceland man braid, pour-over meh prism 90's praxis hell of.",
             timestamp: Date.now() - 4,
             authorUid: "U1"
         },
         {
-            content: "Bar",
+            content: "Banh mi pickled raclette sartorial organic flexitarian JOMO freegan stumptown bicycle rights try-hard messenger bag chia la croix tote bag.",
             timestamp: Date.now() - 3,
             authorUid: "U2"
         },
         {
-            content: "Whatevs",
+            content: "Mumblecore slow-carb portland vexillologist cloud bread. Vape snackwave photo booth paleo.",
             timestamp: Date.now() - 2,
             authorUid: "U1"
         },
         {
-            content: "Check",
+            content: "Polaroid succulents single-origin coffee umami, skateboard pour-over edison bulb.",
             timestamp: Date.now() - 1,
             authorUid: "U2"
         },
         {
-            content: "It out",
+            content: "Kickstarter palo santo cupping lo-fi farm-to-table, gorpcore knausgaard.",
             timestamp: Date.now(),
             authorUid: "U1"
         }
